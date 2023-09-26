8064efa7663f410105677bb3c2d21611a0fc90d4
Windu Sayles <windu.sayles@gmail.com>
2023-09-26 14:35:22 -0700

    Track earned, spent, and total

--- src/core/Scoreboard.ts
+++ src/core/Scoreboard.ts
@@ -5,9 +5,11 @@ export default class Coinsboard {
     public outOfMoney = false;
     private winAmountText: PIXI.Text;
     private winTotal: PIXI.Text;
+    private spentText: PIXI.Text;
     private winAmount: number = 0;
     private money: number = 10;
     private wager: number = 1;
+    private spent: number = 0;
 
     constructor(app: PIXI.Application) {
         this.container = new PIXI.Container();
@@ -17,7 +19,9 @@ export default class Coinsboard {
     decrement() {
         if (!this.outOfMoney) {
             this.money -= this.wager;
-            this.winTotal.text = `Coins: ${this.money}`;
+            this.winTotal.text = `Total: ${this.money}`;
+            this.spent -= this.wager;
+            this.spentText.text = `Spent: ${this.spent * -1}`;
         }
         if (this.money - this.wager < 0) {
             this.outOfMoney = true;
@@ -26,9 +30,9 @@ export default class Coinsboard {
 
     increment(multiplier: number) {
         this.money += this.wager * multiplier;
-        this.winTotal.text = `Coins: ${this.money}`;
-        this.winAmount += this.wager;
-        this.winAmountText.text = `Total: ${this.winAmount}`;
+        this.winTotal.text = `Total: ${this.money}`;
+        this.winAmount += this.wager * multiplier;
+        this.winAmountText.text = `Earned: ${this.winAmount}`;
         if (this.outOfMoney) this.outOfMoney = false;
     }
 
@@ -39,25 +43,26 @@ export default class Coinsboard {
             fill: '#ff33ee',
         });
 
-        this.winTotal = new PIXI.Text(`Coins: ${this.money}`, style);
+        this.winTotal = new PIXI.Text(`Total: ${this.money}`, style);
         this.winTotal.y = 5;
 
-        const wagerText = new PIXI.Text(`Wager: ${this.wager}`, style);
-        wagerText.y = this.winTotal.height + 10;
+        // const wagerText = new PIXI.Text(`Wager: ${this.wager}`, style);
+        this.spentText = new PIXI.Text(`Spent: ${this.spent * -1}`, style);
+        this.spentText.y = this.winTotal.height + 10;
 
-        this.winAmountText = new PIXI.Text(`Total: ${this.winAmount}`, style);
-        this.winAmountText.y = wagerText.y + wagerText.height + 5;
+        this.winAmountText = new PIXI.Text(`Earned: ${this.winAmount}`, style);
+        this.winAmountText.y = this.spentText.y + this.spentText.height + 5;
 
-        wagerText.x = this.winTotal.x = this.winAmountText.x = 10;
+        this.spentText.x = this.winTotal.x = this.winAmountText.x = 10;
 
         const rect = new PIXI.Graphics();
         rect.beginFill(0x02474E, 0.6);
-        const rectHeight = this.winTotal.height + wagerText.height + this.winAmountText.height + 25;
+        const rectHeight = this.winTotal.height + this.spentText.height + this.winAmountText.height + 25;
         rect.drawRect(0, 0, 160, rectHeight);
         rect.endFill();
 
         this.container.x = appWidth - rect.width - 150;
         this.container.y = appHeight / 2 - 25;
-        this.container.addChild(rect, this.winTotal, wagerText, this.winAmountText);
+        this.container.addChild(rect, this.winTotal, this.spentText, this.winAmountText);
     }
 }
