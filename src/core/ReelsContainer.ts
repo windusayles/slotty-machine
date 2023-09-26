6f3b57d61653f6a5ab83867da7fa279c963e7470
Windu Sayles <windu.sayles@gmail.com>
2023-09-26 14:34:57 -0700

    Add win check in all 8 directions

--- src/core/ReelsContainer.ts
+++ src/core/ReelsContainer.ts
@@ -38,9 +38,38 @@ export default class ReelsContainer {
 
         // reel.sprites[2] - Middle visible symbol of the reel
         // we can check for multiple win conditions, and return if any are true, OR set different weights for certain wins, and display the total.
-        // checkForWin needs to return 0 if no wins, a
-        // TODO
-        return this.checkForWin(this.reels.map(reel => reel.sprites[2]));
+        // checkForWin needs to return 0 if no wins,
+        let winTotal = 0;
+
+        // the map returns an array of the 3rd item in each of three vertical reels,
+        // 3rd because 1st is offscreen.
+        winTotal += this.checkForWin(this.reels.map(reel => reel.sprites[2]));
+
+        // a win from top or bottom row counts for half, same as all vertical wins
+        winTotal += (this.checkForWin(this.reels.map(reel => reel.sprites[1])));
+        winTotal += (this.checkForWin(this.reels.map(reel => reel.sprites[3])));
+
+        // lose the first item bc it's offscreen
+        const onScreenReels = this.reels.map(reel => {
+            const newArray = [];
+            for (let i = 1; i < reel.sprites.length; i++) {
+                newArray.push(reel.sprites[i]);
+            }
+            return newArray;
+        })
+        // add both diagonals manually
+        onScreenReels.push([this.reels[0].sprites[1], this.reels[1].sprites[2], this.reels[2].sprites[3]]);
+        onScreenReels.push([this.reels[0].sprites[3], this.reels[1].sprites[2], this.reels[2].sprites[1]]);
+
+        winTotal += (this.checkForWin(onScreenReels[0]));
+        winTotal += (this.checkForWin(onScreenReels[1]));
+        winTotal += (this.checkForWin(onScreenReels[2]));
+        winTotal += (this.checkForWin(onScreenReels[3]));
+        winTotal += (this.checkForWin(onScreenReels[4]));
+        console.log(winTotal);
+
+        return winTotal;
+        // return this.checkForWin(this.reels.map(reel => reel.sprites[2]));
     }
 
     private async* infiniteSpinning(reelsToSpin: Array<Reel>) {
@@ -54,18 +83,16 @@ export default class ReelsContainer {
 
     private checkForWin(symbols: Array<PIXI.Sprite>): number {
         // Set of strings: 'SYM1', 'SYM2', ...
-        //
         const combination: Set<string> = new Set();
         symbols.forEach(symbol => combination.add(symbol.texture.textureCacheIds[0].split('.')[0]));
-        // always win, for dev testing
-        // return true;
 
         // all match, so combo size is 1, and does not have wild card bc all 3 wild card
-        // if (combination.size === 1 && !combination.has('SYM1')) return true;
+        // if (combination.size === 1 && !combination.has('SYM1')) return 0;
 
-        // wild card win is triple, not double
-        if (combination.size === 1 && !combination.has('SYM1')) return 3;
-        return combination.size === 2 && combination.has('SYM1') ? 2 : 0;
+        // all wild card win is doubled
+        if (combination.size === 1 && !combination.has('SYM1')) return 2;
+
+        return combination.size === 2 && combination.has('SYM1') ? 1 : 0;
     }
 
     private blessRNG() {
